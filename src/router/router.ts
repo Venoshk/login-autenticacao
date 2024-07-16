67eeac59cb8fdb8671c13c558dd43c20915165ee
Henrique <venoshk.henrique19@gmail.com>
2024-07-15 23:47:42 -0300

    Rotas

--- src/router/router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { UserController } from "../controllers/UserControllers";
-
-export const router = Router();
-
-router.get("/", new UserController().getProfile);
-router.post("/register", new UserController().create);
-router.post("/login", new  UserController().login);
